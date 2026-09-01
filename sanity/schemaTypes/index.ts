d951655f95afe4e12ced6d2ef9bd3faa89bbb607
lsilva-17 <pauliino1996@gmail.com>
2026-09-01 09:59:01 -0300

    feat(v2): register CMS schemas

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { caseStudy } from './caseStudy';
+import { siteSettings } from './siteSettings';
+import { treatment } from './treatment';
+
+export const schemaTypes = [siteSettings, treatment, caseStudy];
